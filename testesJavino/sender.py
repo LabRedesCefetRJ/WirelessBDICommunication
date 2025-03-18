c8fc85087f17987d11cc34458aefdcb76462e046
AntonioHenriqueGF <hgfantonio@gmail.com>
2025-03-18 20:27:32 -0300

    refactor: Migrate files from Python 3.11 to 3.12 and clean up unused binaries

--- testesJavino/sender.py
+++ testesJavino/sender.py
@@ -1,28 +1,26 @@
 import javino
+import time
+
+TIMEOUT = 3
 
 if __name__ == "__main__":
-    porta = "/dev/ttyEmulatedPort0"  # Substitua pela porta serial desejada
+    porta = "/dev/ttyUSB0"  # Substitua pela porta serial desejada
     comm = javino.start(porta)
     
     if comm:
-        try:
-            javino.addPercept("valores")
-            javino.sendPercepts(comm)
-            # while True:
-            #     # Verifica se há mensagens disponíveis na porta serial
-            #     if javino.availableMsg(comm):
-            #         mensagem = javino.getMsg(comm)                   
-            #         if mensagem == "getPercepts":
-            #             getExogenousPerceptions()
-            #             print("sending perceptions")
-            #             javino.sendPercepts(comm)
-            #         elif mensagem == "ledOn":
-            #             ledOn()
-            #         elif mensagem == "ledOff":
-            #             ledOff()
-            #         else:
-            #             print(f"Action not implemented:{mensagem}")
-        except KeyboardInterrupt:
-            javino.disconnect(comm)
+        while True:
+            try:
+                javino.addPercept("valores")
+                javino.sendPercepts(comm)
+                start_time = time.time()
+                while time.time() - start_time < TIMEOUT:
+                    # Verifica se há mensagens disponíveis na porta serial
+                    if javino.availableMsg(comm):
+                        mensagem = javino.getMsg(comm)
+                        print(f"Mensagem: {mensagem}")
+            except KeyboardInterrupt:
+                javino.disconnect(comm)
+            except:
+                print("Timeout!")
     else:
         print("Não foi possível conectar à porta serial.")
