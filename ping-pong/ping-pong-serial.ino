40625689ec5c06c662d0ef67307cdfa7df3ea090
AntonioHenriqueGF <hgfantonio@gmail.com>
2025-06-20 00:59:23 -0300

    Remove unnecessary comments from ping-pong-serial.ino

--- ping-pong/ping-pong-serial.ino
+++ ping-pong/ping-pong-serial.ino
@@ -2,13 +2,11 @@
 SoftwareSerial IOPort(11,10); // RX, TX
 
 void setup() {
-  // put your setup code here, to run once:
   IOPort.begin(9600);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if (Serial.available()) {
     IOPort.print(Serial.readString());
   }
